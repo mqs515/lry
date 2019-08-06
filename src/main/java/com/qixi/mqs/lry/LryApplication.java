ffbd5809631ed7b9521eec82ce4e4120674723c3
miaoqs <quansheng.miao@fansunion.com>
2019-08-06 17:38:23 +0800

    Initial commit

---
+++ src/main/java/com/qixi/mqs/lry/LryApplication.java
@@ -0,0 +1,13 @@
+package com.qixi.mqs.lry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LryApplication.class, args);
+    }
+
+}
